a2bf3a7ed34dfaac9cdecd2b2027bb5a1b442864
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-11-25 05:45:42 +0500

    object

--- 2411/index.js
+++ 2411/index.js
@@ -1,43 +1,81 @@
-// function evenOrOdd(number){
-//     if(number % 2 == 1) {
-//        console.log("Odd");
-//     }
-//     else
-//     {
-//         console.log("Even");
-// }
-// }
+// // function evenOrOdd(number){
+// //     if(number % 2 == 1) {
+// //        console.log("Odd");
+// //     }
+// //     else
+// //     {
+// //         console.log("Even");
+// // }
+// // }
 
-// evenOrOdd(45);
-
-
+// // evenOrOdd(45);
 
 
 
- //=========
 
-// helloWorld = function() {
-// var str = "Hello World!";
-// console.log( str );
-//   }
-//   helloWorld();
 
-// function sameCase(a){}
+//  //=========
 
-// sameCase([
-//     {bill:12},
-//     {sam:34,child:[{childAge:13}]},
-//     {sam: 3},
-//     {sam:32},
-// ])
-function star(a){
-    if(a % 2 == 1){
-        for(let i = 1; i <= a ; i++){
+// // helloWorld = function() {
+// // var str = "Hello World!";
+// // console.log( str );
+// //   }
+// //   helloWorld();
+
+// // function sameCase(a){}
+
+// // sameCase([
+// //     {bill:12},
+// //     {sam:34,child:[{childAge:13}]},
+// //     {sam: 3},
+// //     {sam:32},
+// // ])
+// function star(a){
+//     if(a % 2 == 1){
+//         for(let i = 1; i <= a ; i++){
          
-        }
-    }
-    else{
-        console.log('you can not' );
-    }
-}
-star(5);
+//         }
+//     }
+//     else{
+//         console.log('you can not' );
+//     }
+// }
+// star(5);
+
+
+
+
+const account1 = {
+    name:'Webbrain',
+    major :'Frontend'
+};
+
+const account2 = account1;
+   
+console.log(account1 === account2);
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
