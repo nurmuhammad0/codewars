645ebc51978822fcd737585c8f610197badf83d7
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-11-26 23:15:56 +0500

    function

--- 2411/index.js
+++ 2411/index.js
@@ -1,46 +1,46 @@
-// // function evenOrOdd(number){
-// //     if(number % 2 == 1) {
-// //        console.log("Odd");
-// //     }
-// //     else
-// //     {
-// //         console.log("Even");
-// // }
-// // }
-
-// // evenOrOdd(45);
-
-
-
-
-
-//  //=========
-
-// // helloWorld = function() {
-// // var str = "Hello World!";
-// // console.log( str );
-// //   }
-// //   helloWorld();
-
-// // function sameCase(a){}
-
-// // sameCase([
-// //     {bill:12},
-// //     {sam:34,child:[{childAge:13}]},
-// //     {sam: 3},
-// //     {sam:32},
-// // ])
-// function star(a){
-//     if(a % 2 == 1){
-//         for(let i = 1; i <= a ; i++){
-         
-//         }
+// function evenOrOdd(number){
+//     if(number % 2 == 1) {
+//        console.log("Odd");
 //     }
-//     else{
-//         console.log('you can not' );
-//     }
+//     else
+//     {
+//         console.log("Even");
 // }
-// star(5);
+// }
+
+// evenOrOdd(45);
+
+
+
+
+
+ //=========
+
+// helloWorld = function() {
+// var str = "Hello World!";
+// console.log( str );
+//   }
+//   helloWorld();
+
+// function sameCase(a){}
+
+// sameCase([
+//     {bill:12},
+//     {sam:34,child:[{childAge:13}]},
+//     {sam: 3},
+//     {sam:32},
+// ])
+function star(a){
+    if(a % 2 == 1){
+        for(let i = 1; i <= a ; i++){
+         
+        }
+    }
+    else{
+        console.log('you can not' );
+    }
+}
+star(5);
 
 
 
---
+++ 2611/index.js
@@ -0,0 +1,30 @@
+
+// function min(list){
+//     var min =list[0];
+//     for(var i=0; i<list.length; i++){
+//        var cur = list[i];
+//        if (cur<min) min=cur;
+//     }
+//     return min;
+// }
+// console.log(min([4,6,2,1,9,63,-134,566]));
+
+// function max(list){
+//     var max =list[0];
+//     for(var i=0; i<list.length; i++){
+//        var cur = list[i];
+//        if (cur>max) max=cur;
+//     }
+//     return max;
+// }
+// console.log(max([4,6,2,1,9,63,-134,566]));
+
+
+
+// function invert(array) {
+//     for(let i=0; i<array.length; i++){
+//       array[i] *= -1;
+//     }
+//      return array;
+//   }
+//   console.log(invert([1,2,3,4,5]));
